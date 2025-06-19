430e290e98e0eae0d448fc7cebb804d22ec50014
irule4567 <kywilliford02@gmail.com>
2025-06-19 12:25:24 -0400

    Initial commit
    Initial commit containing a largely functional Chronos Pendant. Still need to make a proper model for it.

---
+++ Smite Items/Items/ChronosPendant.cs
@@ -0,0 +1,166 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using static Smite_Items.Main;
+
+namespace Smite_Items.Items
+{
+    public class ChronosPendant : ItemBase<ChronosPendant>
+    {
+        public ConfigEntry<float> ItemCooldown;
+        public override string ItemName => "Chronos Pendant";
+
+        public override string ItemLangTokenName => "CHRONOS_PENDANT";
+
+        public override string ItemPickupDesc => "Periodically lower cooldowns by 1 second.";
+
+        public override string ItemFullDescription => "Every 10 seconds, lower all ability cooldowns by 1 second";
+
+        public override string ItemLore => "Item taken from Smite 2";
+
+        public override ItemTier Tier => ItemTier.Tier2;
+
+        public override GameObject ItemModel => MainAssets.LoadAsset<GameObject>("ChronosPendantDisplay.prefab");
+
+        public override Sprite ItemIcon => MainAssets.LoadAsset<Sprite>("Chronos Pendant Icon.png");
+
+        public static BuffDef chronosPendantCooldown;
+
+        public static float secondsRemovedPerActivation = 1f;
+
+        public static float chronosPendantCooldownDuration = 10f;
+
+        public static GameObject ItemBodyModelPrefab;
+
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateItem();
+            CreateBuff();
+            Hooks();
+        }
+
+        public override void CreateConfig(ConfigFile config)
+        {
+            ItemCooldown = config.Bind<float>("Item " + ItemName, "Item Cooldown", 10, "How many seconds between each item proc?");
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.CharacterBody.OnInventoryChanged += CharacterBody_OnInventoryChanged;
+            //On.RoR2.CharacterBody.FixedUpdate += ChronosCooldown;
+        }
+
+        private void CharacterBody_OnInventoryChanged(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self)
+        {
+            orig(self);
+            //On.RoR2.CharacterBody.FixedUpdate += ChronosCooldown;
+
+            self.AddItemBehavior<ChronosPendantBehavior>(GetCount(self));
+        }
+
+        /*private void ChronosCooldown(On.RoR2.CharacterBody.orig_FixedUpdate orig, CharacterBody self)
+        {
+            orig(self);
+            var inventoryCount = GetCount(self);
+            if (inventoryCount > 0 && self && !self.HasBuff(chronosPendantCooldown))
+            {
+                Debug.Log("Gets into buff check");
+                if (self.skillLocator)
+                {
+                    Debug.Log("Adds buff");
+                    self.skillLocator.DeductCooldownFromAllSkillsServer(secondsRemovedPerActivation);
+                    for (int k = 1; (float)k <= chronosPendantCooldownDuration; k++)
+                    {
+                        Debug.Log("Adds buff of " + k + " second duration");
+                        self.AddTimedBuff(chronosPendantCooldown, chronosPendantCooldownDuration);
+                    }
+                }
+            }
+        }*/
+
+        public void CreateBuff()
+        {
+            chronosPendantCooldown = ScriptableObject.CreateInstance<BuffDef>();
+            chronosPendantCooldown.canStack = true;
+            chronosPendantCooldown.isDebuff = false;
+            chronosPendantCooldown.name = "ChronosPendantCooldown";
+            chronosPendantCooldown.isCooldown = true;
+            chronosPendantCooldown.iconSprite = Addressables.LoadAssetAsync<Sprite>("Chronos Pendant Icon.png").WaitForCompletion();
+            ContentAddition.AddBuffDef(chronosPendantCooldown);
+        }
+
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+    }
+
+    public class ChronosPendantBehavior : CharacterBody.ItemBehavior
+    {
+        private float cooldownTimer;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void OnEnable()
+        {
+            cooldownTimer = 0f;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!body || !body.skillLocator)
+                return;
+
+            int currentStacks = body.GetBuffCount(ChronosPendant.chronosPendantCooldown);
+
+            if (currentStacks > 0)
+            {
+                return;
+            }
+
+            float reduction = ChronosPendant.secondsRemovedPerActivation;
+            body.skillLocator.DeductCooldownFromAllSkillsServer(reduction);
+
+            //body.SetBuffCount(ChronosPendant.chronosPendantCooldown.buffIndex, (int)ChronosPendant.chronosPendantCooldownDuration);
+
+            for (int k = 1; (int)k <= ChronosPendant.chronosPendantCooldownDuration; k++)
+            {
+                body.AddTimedBuff(ChronosPendant.chronosPendantCooldown, k);
+            }
+            /*if (!body.HasBuff(ChronosPendant.chronosPendantCooldown))
+            {
+                cooldownTimer -= Time.deltaTime;
+
+                if (cooldownTimer <= 0f)
+                {
+                    cooldownTimer = ChronosPendant.chronosPendantCooldownDuration;
+
+                    float reduction = ChronosPendant.secondsRemovedPerActivation;
+                    body.skillLocator.DeductCooldownFromAllSkillsServer(reduction);
+                    for (int k = 1; (int)k <= ChronosPendant.chronosPendantCooldownDuration; k++)
+                    {
+                        Debug.Log("Adds buff of " + k + " second duration");
+                        body.AddTimedBuff(ChronosPendant.chronosPendantCooldown, k);
+                    }
+                }
+            }
+            else
+            {
+                cooldownTimer = 0f;
+            }*/
+
+        }
+    }
+}
