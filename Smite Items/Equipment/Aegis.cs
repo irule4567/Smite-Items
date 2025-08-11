622d5b05ececdd7546847b72bbafcf6f865267f0
irule4567 <kywilliford02@gmail.com>
2025-08-11 13:20:17 -0400

    Started Aegis Amulet
    Set up basic file for Aegis Amulet.

---
+++ Smite Items/Equipment/Aegis.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using UnityEngine;
+using static Smite_Items.Main;
+
+namespace Smite_Items.Equipment
+{
+    public class Aegis : EquipmentBase
+    {
+        public ConfigEntry<float> InvulnDuration;
+        public override string EquipmentName => "Aegis Amulet";
+
+        public override string EquipmentLangTokenName => "AEGIS_AMULET";
+
+        public override string EquipmentPickupDesc => "Grants temporary invulernability";
+
+        public override string EquipmentFullDescription => $"Become invulnerable for <style=cIsUtility>{InvulnDuration.Value}</style> seconds.";
+
+        public override string EquipmentLore => "";
+
+        public override GameObject EquipmentModel => MainAssets.LoadAsset<GameObject>("ExampleEquipmentPrefab.prefab");
+
+        public override Sprite EquipmentIcon => MainAssets.LoadAsset<Sprite>("ExampleEquipmentIcon.png");
+
+        public override void Init(ConfigFile config)
+        {
+            CreateConfig(config);
+            CreateLang();
+            CreateEquipment();
+            Hooks();
+        }
+
+        protected override void CreateConfig(ConfigFile config)
+        {
+            InvulnDuration = config.Bind<float>("Equipment " + EquipmentName, "Number of seconds of invulnerability from the equipment", 1.5f, "How many seconds does the equipment effect last?");
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        protected override bool ActivateEquipment(EquipmentSlot slot)
+        {
+            return false;
+        }
+
+
+    }
+}
--- Smite Items/obj/Debug/netstandard2.1/Smite Items.AssemblyInfo.cs
+++ Smite Items/obj/Debug/netstandard2.1/Smite Items.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smite Items")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41352f47316c80f3edda605c91775926ea613e06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd997480fc4d795e01cf5d420653c69f41591662")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smite Items")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smite Items")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
