60626a365b940507bfdcc4e79c9a933443c62471
irule4567 <kywilliford02@gmail.com>
2025-11-21 13:11:35 -0500

    Finished Phalanx
    Finished the functionality of Phalanx; Now additional stacks give more buff stacks when hit and buff stacks are applied to allies in range when hit.

--- Smite Items/Items/Phalanx.cs
+++ Smite Items/Items/Phalanx.cs
@@ -4,6 +4,7 @@ using R2API;
 using RoR2;
 using Smite_Items.Utils;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.Networking;
@@ -14,20 +15,20 @@ namespace Smite_Items.Items
     public class Phalanx : ItemBase<Phalanx>
     {
         public ConfigEntry<float> AttackSpeedBonus;
-        public ConfigEntry<float> AttackSpeedBonusPerStack;
+        //public ConfigEntry<float> AttackSpeedBonusPerStack;
         public ConfigEntry<int> MaxBuffStacks;
         public ConfigEntry<int> MaxBuffStacksPerStack;
         public ConfigEntry<float> BuffRadius;
         public ConfigEntry<int> BuffDuration;
+        //public ConfigEntry<int> BuffDurationPerStack;
 
         public override string ItemName => "Phalanx";
 
         public override string ItemLangTokenName => "PHALANX_ITEM";
 
-        public override string ItemPickupDesc => "Gain attack speed when damaged. Share it with nearby allies.";
+        public override string ItemPickupDesc => "You and nearby allies gain attack speed when you take damage.";
 
-        public override string ItemFullDescription => $"Getting hit creates an aura in a <style=cIsDamage>{BuffRadius.Value}m</style> radius around you that increases <style=cIsDamage>attack speed</style> by <style=cIsDamage>{AttackSpeedBonus.Value*100}%</style> <style=cStack>(+{AttackSpeedBonusPerStack.Value*100}% per stack)</style> for all allies that lasts <style=cIsDamage>{BuffDuration.Value}s</style>." +
-            $" Maximum cap of <style=cIsDamage>{MaxBuffStacks.Value*AttackSpeedBonus.Value*100}%</style> <style=cStack>(+{MaxBuffStacksPerStack.Value * (AttackSpeedBonus.Value + AttackSpeedBonusPerStack.Value) * 100}% per stack) <style=cIsDamage>attack speed</style>.";
+        public override string ItemFullDescription => $"Increase the <style=cIsDamage>attack speed</style> of you and allies in a <style=cIsDamage>{BuffRadius.Value}m</style> radius around you by <style=cIsDamage>{AttackSpeedBonus.Value * 100}%</style> <style=cStack>(+{AttackSpeedBonus.Value * 100}% per stack)</style> when you get hit up to <style=cIsDamage>{MaxBuffStacks.Value}</style> <style=cStack>(+{MaxBuffStacksPerStack.Value} per stack)</style> <style=cIsDamage>times</style> for <style=cIsDamage>{BuffDuration.Value}s</style>.";
 
         public override string ItemLore => "Item taken from Smite 1.";
 
@@ -39,7 +40,9 @@ namespace Smite_Items.Items
 
         public static BuffDef attackSpeedOnDamageBuff;
 
-        public static GameObject radiusIndicator;
+        //public static GameObject radiusIndicator;
+
+        public static Dictionary<CharacterBody,GameObject> radiusIndicators = new Dictionary<CharacterBody, GameObject>();
 
         public override void Init(ConfigFile config)
         {
@@ -60,12 +63,13 @@ namespace Smite_Items.Items
 
         public override void CreateConfig(ConfigFile config)
         {
-            AttackSpeedBonus = config.Bind<float>("Item " + ItemName, "Attack Speed Buff", 0.1f, "How much does each buff stack increase attack speed by?");
-            AttackSpeedBonusPerStack = config.Bind<float>("Item " + ItemName, "Attack Speed Buff increase per stack", 0.05f, "How much does each additional stack of the item increase the attack speed buff by?");
+            AttackSpeedBonus = config.Bind<float>("Item " + ItemName, "Attack Speed Buff", 0.075f, "How much does each buff stack increase attack speed by?");
+            //AttackSpeedBonusPerStack = config.Bind<float>("Item " + ItemName, "Attack Speed Buff increase per stack", 0.05f, "How much does each additional stack of the item increase the attack speed buff by?");
             MaxBuffStacks = config.Bind<int>("Item " + ItemName, "Max stacks of buff", 3, "What is the maximum number of stacks of the attack speed buff a character can have at once?");
-            MaxBuffStacksPerStack = config.Bind<int>("Item " + ItemName, "Additional max stacks of buff per stack", 1, "How many additional stacks of the buff are allowed per additional stack of the item?");
-            BuffRadius = config.Bind<float>("Item " + ItemName, "Buff Radius", 40, "What is the radius, in meters, in which the attack speed buff is shared?");
+            MaxBuffStacksPerStack = config.Bind<int>("Item " + ItemName, "Additional max stacks of buff per stack", 3, "How many additional stacks of the buff are allowed per additional stack of the item?");
+            BuffRadius = config.Bind<float>("Item " + ItemName, "Buff Radius", 20, "What is the radius, in meters, in which the attack speed buff is shared?");
             BuffDuration = config.Bind<int>("Item " + ItemName, "Buff Duration", 10, "How long, in seconds, does the item buff last?");
+            //BuffDurationPerStack = config.Bind<int>("Item " + ItemName, "Added Buff Duration per stack", 5, "How long, in seconds, does each additional stack of the item increase the buff duration by?");
         }
 
         public void CreateBuff()
@@ -96,41 +100,74 @@ namespace Smite_Items.Items
             On.RoR2.HealthComponent.TakeDamage += ApplyPhalanxBuff;
             RecalculateStatsAPI.GetStatCoefficients += ApplyBuffStats;
             On.RoR2.CharacterBody.OnInventoryChanged += AddIndicator;
+            On.RoR2.CharacterBody.OnDestroy += CleanupIndicator;
+            //Stage.onStageStartGlobal += CleanupInicatorsStage;
+        }
+
+        /*private void CleanupInicatorsStage(Stage stage)
+        {
+            
+            radiusIndicators.Clear();
+        }*/
+
+        private void CleanupIndicator(On.RoR2.CharacterBody.orig_OnDestroy orig, CharacterBody self)
+        {
+            orig(self);
+            if (radiusIndicators.TryGetValue(self, out GameObject existingIndicator))
+            {
+                if (existingIndicator != null)
+                {
+                    UnityEngine.Object.Destroy(existingIndicator);
+                }
+                radiusIndicators.Remove(self);
+            }
         }
 
         private void AddIndicator(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self)
         {
             orig(self);
+            if (!NetworkServer.active) return;
             var stackCount = GetCount(self);
-            if (stackCount > 0 && radiusIndicator == null)
+            if (stackCount > 0)
             {
-                GameObject original = LegacyResourcesAPI.Load<GameObject>("Prefabs/NetworkedObjects/NearbyDamageBonusIndicator");
-                radiusIndicator = UnityEngine.Object.Instantiate(original, self.corePosition, Quaternion.identity);
-                radiusIndicator.transform.localScale *= (BuffRadius.Value/13f)*2;
-                var renderer = radiusIndicator.GetComponentInChildren<Renderer>();
-                if(renderer != null)
+                if (!radiusIndicators.ContainsKey(self) || radiusIndicators[self] == null)
                 {
-                    Material mat = new Material(renderer.material);
-                    Color newColor = new Color(0.5f, 0.2f, 1f, 0.3f);
-                    mat.SetColor("_TintColor", newColor);
-                    renderer.material = mat;
+                    GameObject original = LegacyResourcesAPI.Load<GameObject>("Prefabs/NetworkedObjects/NearbyDamageBonusIndicator");
+                    GameObject radiusIndicator = UnityEngine.Object.Instantiate(original, self.corePosition, Quaternion.identity);
+                    radiusIndicator.transform.localScale *= (BuffRadius.Value / 13f) * 2;
+                    var renderer = radiusIndicator.GetComponentInChildren<Renderer>();
+                    if (renderer != null)
+                    {
+                        //Material mat = new Material(renderer.material);
+                        MaterialPropertyBlock propBlock = new MaterialPropertyBlock();
+                        renderer.GetPropertyBlock(propBlock);
+                        Color newColor = new Color(0.5f, 0.2f, 1f, 0.3f);
+                        propBlock.SetColor("_TintColor", newColor);
+                        renderer.SetPropertyBlock(propBlock);
+                        //renderer.material = mat;
+                    }
+                    radiusIndicator.GetComponent<NetworkedBodyAttachment>().AttachToGameObjectAndSpawn(self.gameObject);
+                    radiusIndicators[self] = radiusIndicator;
                 }
-                radiusIndicator.GetComponent<NetworkedBodyAttachment>().AttachToGameObjectAndSpawn(self.gameObject);
             }
-            else if(stackCount == 0 && radiusIndicator != null)
+            else if(radiusIndicators.TryGetValue(self, out GameObject existingIndicator))
             {
-                UnityEngine.Object.Destroy(radiusIndicator);
-                radiusIndicator=null;
+                if (existingIndicator != null)
+                {
+                    UnityEngine.Object.Destroy(existingIndicator);
+                }
+                radiusIndicators.Remove(self);
             }
         }
 
         private void ApplyBuffStats(CharacterBody sender, RecalculateStatsAPI.StatHookEventArgs args)
         {
+            if (sender == null) return;
             int buffCount = sender.GetBuffCount(attackSpeedOnDamageBuff);
-            var stackCount = GetCount(sender);
-            if (buffCount > 0) // Set movement speed based on stacks of buffs
+            //var stackCount = GetCount(sender);
+            if (buffCount > 0) // Set attack speed based on stacks of buffs
             {
-                args.attackSpeedMultAdd += buffCount * (AttackSpeedBonus.Value + ((stackCount-1) * AttackSpeedBonusPerStack.Value));
+                args.attackSpeedMultAdd += buffCount * AttackSpeedBonus.Value; /*+ ((stackCount-1) * AttackSpeedBonusPerStack.Value));*/
             }
         }
 
@@ -143,13 +180,38 @@ namespace Smite_Items.Items
                 var stackCount = GetCount(self.body);
                 if (stackCount > 0)
                 {
-                    var buffCount = self.body.GetBuffCount(attackSpeedOnDamageBuff);
-                    var maxBuffStacks = MaxBuffStacks.Value + ((stackCount - 1) * MaxBuffStacksPerStack.Value);
-                    if(buffCount < maxBuffStacks)
+                    TeamIndex teamIndex = self.body.teamComponent.teamIndex;
+                    SphereSearch sphereSearch = new SphereSearch
                     {
-                        self.body.AddTimedBuff(attackSpeedOnDamageBuff, BuffDuration.Value);
+                        mask = LayerIndex.entityPrecise.mask,
+                        origin = self.body.corePosition,
+                        queryTriggerInteraction = QueryTriggerInteraction.Collide,
+                        radius = BuffRadius.Value
+                    };
+                    TeamMask teamMask = default(TeamMask);
+                    teamMask.AddTeam(teamIndex);
+                    List<HurtBox> hurtBoxesList = new List<HurtBox>();
+                    sphereSearch.RefreshCandidates().FilterCandidatesByHurtBoxTeam(teamMask).FilterCandidatesByDistinctHurtBoxEntities().GetHurtBoxes(hurtBoxesList);
+                    foreach (HurtBox hurtBox in hurtBoxesList)
+                    {
+                        HealthComponent healthComponent = hurtBox.healthComponent;
+                        if (healthComponent)
+                        {
+                            var buffCount = healthComponent.body.GetBuffCount(attackSpeedOnDamageBuff);
+                            var maxBuffStacks = MaxBuffStacks.Value + ((stackCount - 1) * MaxBuffStacksPerStack.Value);
+                            for (int j = 0; j < stackCount; j++) // Add a buff stack per stack of the item
+                            {
+                                if (buffCount < maxBuffStacks)
+                                {
+                                    healthComponent.body.AddTimedBuff(attackSpeedOnDamageBuff, BuffDuration.Value);
+                                    buffCount = healthComponent.body.GetBuffCount(attackSpeedOnDamageBuff);
+                                }
+                            }
+                            ItemHelpers.RefreshTimedBuffs(healthComponent.body, attackSpeedOnDamageBuff, BuffDuration.Value);
+                        }
                     }
-                    ItemHelpers.RefreshTimedBuffs(self.body, attackSpeedOnDamageBuff, BuffDuration.Value);
+                    hurtBoxesList.Clear();
+                    
                 }
             }
         }
--- Smite Items/obj/Debug/netstandard2.1/Smite Items.AssemblyInfo.cs
+++ Smite Items/obj/Debug/netstandard2.1/Smite Items.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smite Items")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84df3cef28dccf2d5f619fdf07364ce5ba0a25c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165466e79b282d86403f440a9d47bd145870d45e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smite Items")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smite Items")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
